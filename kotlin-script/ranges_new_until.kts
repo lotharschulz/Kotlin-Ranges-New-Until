9ae7393307b67a587496febcf4ee9dc77b53288a
Lothar Schulz <lothar.sch@gmx.de>
2022-10-02 22:27:22 +0200

    chore kotlin script reorg

--- kotlin-script/ranges_new_until.kts
+++ kotlin-script/ranges_new_until.kts
@@ -1,7 +1,8 @@
 val A_TO_D = ('a'..'d')
 val A_UNTIL_D = ('a' until 'd')
 val A_RANGETO_D = ('a'.rangeTo('d'))
-//val A_RANGETO_D_NEW = ('a'..<'d')
+@OptIn(kotlin.ExperimentalStdlibApi::class)
+val A_NEW_UNTIL_D = ('a'..<'d')
 val A_DOWNTO_D = ('d'.downTo('a'))
 
 val dotdotRange: String
@@ -12,14 +13,14 @@ val untilRange: String
     get() {
         return A_UNTIL_D.joinToString(",")
     }
+val rangeRangeNew_1_7_20: String
+    get() {
+        return A_NEW_UNTIL_D.joinToString(",")
+    }
 val rangeTo: String
     get() {
         return A_RANGETO_D.joinToString(",")
     }
-//val rangeToNew_1_7_20: String
-//    get() {
-//        return A_RANGETO_D_NEW.joinToString(",")
-//    }
 val downTo: String
     get() {
         return A_DOWNTO_D.joinToString(",")
@@ -29,6 +30,6 @@ println(
     "RangedotdotRange: ${dotdotRange} \n" +
     "untilRange: ${untilRange} \n" +
     "rangeTo: ${rangeTo} \n" +
-//    "rangeTo new 1.7.20: ${rangeToNew_1_7_20} \n" +
+    "untilRange new 1.7.20: ${rangeRangeNew_1_7_20} \n" +
     "downTo: ${downTo} \n"
 )
